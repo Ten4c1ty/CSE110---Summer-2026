f4ea20b908dd05594937684dd1f6ecaeba0b503e
Ten4c1ty <shayan09ashraf@gmail.com>
2026-09-12 21:02:08 +0600

    Add CW02.java with basic arithmetic and string operations
    Basic Data Types and how they work in action.

---
+++ CW02.java
@@ -0,0 +1,43 @@
+public class CW02{
+    public static void main(String args[]){
+        // 2.1
+        int a = 10;
+        System.out.println(a);// 10
+        // 2.2
+        int b = 13;
+        System.out.println(a+b);// 10 + 13 = 23
+        // 2.3
+        System.out.println(a*b);// 10 X 13 = 130
+        System.out.println(a/b);// 10 / 13 = 0
+        // 2.4
+        double c = 20.0;
+        System.out.println(c);// 20.0
+        double d = 23d;
+        System.out.println(c+d);// 20.0 + 23.0 = 43.0
+        System.out.println(c*d);// 20.0 X 23.0 = 460.0
+        System.out.println(c/d);// 20.0 / 23.0 = 0.8695652173913043
+        // 2.5
+        int f = 40;
+        System.out.println(f);// 40
+        double g = 53d;
+        System.out.println(f+g);// 40.0 + 53.0 = 93.0
+        System.out.println(f*g);// 40.0 X 53.0 = 2120.0
+        System.out.println(f/g);// 40.0 / 53.0 = 0.7547169811320755
+        // 2.6
+        String h = "abc";
+        System.out.println(h);// abc
+        String i = "def";
+        System.out.println(i);// def
+        System.out.println(h+i);// abcdef
+        int j = 10;
+        System.out.println(j);// 10
+        String k = "ghi";
+        System.out.println(k);// ghi
+        System.out.println(j+k);// 10ghi
+        String l = "klj";
+        System.out.println(l);// klj
+        int m = 10;
+        System.out.println(m);// 10
+        System.out.println(l+m);// klj10
+    }
+}

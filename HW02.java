c6e067a0cc6e1d63dfd2a2078c2b7209919239b7
Ten4c1ty <shayan09ashraf@gmail.com>
2026-09-13 00:07:10 +0600

    Add HW02.java with main method and calculations

---
+++ HW02.java
@@ -0,0 +1,7 @@
+public class HW02{
+    public static void main(String args[]){
+        int a = 2, b = 5, c = 8; // Another procedure to declare and initialize variables of same data type in a single line
+        int d = ((2*b)*(c-a)/3)+7;
+        System.out.println(d); // 27
+    }
+}

19718c008745515adf7873f4c3e0ed2ac480bb02
Ten4c1ty <shayan09ashraf@gmail.com>
2026-09-13 00:22:00 +0600

    Implement main method to calculate sine and cosine

---
+++ HW05.java
@@ -0,0 +1,7 @@
+public class HW05{
+    public static void main(String args[]){
+        double a = 4.5, b = 9.5;
+        double c = Math.sqrt(a*a+b*b);
+        System.out.println("Sin(A) = "+a/c+" Cos(A) = "+b/c+" Sin(B) = "+b/c+" Cos(B) = "+a/c);// Sin(A) = 0.42808634473904467 Cos(A) = 0.9037378388935388 Sin(B) = 0.9037378388935388 Cos(B) = 0.42808634473904467
+    }
+}

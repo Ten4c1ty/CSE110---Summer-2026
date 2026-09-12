bfbc2a0b9687cff3e88fb85fb3af921c0fc27107
Ten4c1ty <shayan09ashraf@gmail.com>
2026-09-12 22:45:24 +0600

    Add CW05.java for inch to meter conversion

---
+++ CW05.java
@@ -0,0 +1,7 @@
+public class CW05{
+    public static void main(String args[]){
+        int inch = 1000;
+        double meter = (double)inch * 0.0254; // No need to memorize the conversion factor
+        System.out.println(meter); // 25.4
+    }
+}
